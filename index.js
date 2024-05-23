8b7556c048d0d9c1bf7cb5a5fb613c49457cd4c3
Eli McMillan <100636385+Eli-Zac-09@users.noreply.github.com>
2024-05-23 20:42:12 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
-const fetch = require('node-fetch');
-
-async function simulateXP() {
-    try {
-        const headers = {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${process.env.DUOLINGO_JWT}`,
-            "user-agent":
-                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-        };
-
-        const { sub } = JSON.parse(
-            Buffer.from(process.env.DUOLINGO_JWT.split(".")[1], "base64").toString()
-        );
-
-        const { fromLanguage, learningLanguage } = await fetch(
-            `https://www.duolingo.com/2017-06-30/users/${sub}?fields=fromLanguage,learningLanguage`,
-            { headers }
-        ).then((response) => response.json());
-
-        const xpPerSecond = 50;
-        const durationInSeconds = process.env.DURATION || 60; // Default duration is 60 seconds
-
-        let xp = 0;
-
-        const intervalId = setInterval(() => {
-            xp += xpPerSecond;
-            console.log(`🎉 You won ${xp} XP`);
-
-            if (xp >= durationInSeconds * xpPerSecond) {
-                clearInterval(intervalId); // Stop the interval once the duration is reached
-            }
-        }, 1000); // Run every second
-
-    } catch (error) {
-        console.log("❌ Something went wrong");
-        if (error instanceof Error) {
-            console.log(error.message);
-        }
-        process.exit(1); // Exit with a non-zero code to indicate failure
-    }
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${process.env.DUOLINGO_JWT}`,
+  'user-agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
 }
 
-simulateXP();
+const SESSION_PAYLOAD = {"challengeTypes":["assist","characterIntro","characterMatch","characterPuzzle","characterSelect","characterTrace","completeReverseTranslation","definition","dialogue","form","freeResponse","gapFill","judge","listen","listenComplete","listenMatch","match","name","listenComprehension","listenIsolation","listenTap","partialListen","partialReverseTranslate","patternTapComplete","readComprehension","select","selectPronunciation","selectTranscription","syllableTap","syllableListenTap","speak","tapCloze","tapClozeTable","tapComplete","tapCompleteTable","tapDescribe","translate","transliterate","typeCloze","typeClozeTable","typeCompleteTable","writeComprehension"],"fromLanguage":"en","isFinalLevel":false,"isV2":true,"juicy":true,"learningLanguage":"ja","smartTipsVersion":2,"isCustomIntroSkill":false,"isGrammarSkill":false,"levelIndex":0,"showGrammarSkillSplash":false,"skillId":"060ce4633b12e01d03c5baa22fddd7ab","type":"LESSON","levelSessionIndex":0};
+
+const { sub } = JSON.parse(
+  Buffer.from(process.env.DUOLINGO_JWT.split('.')[1], 'base64').toString(),
+)
+
+const { fromLanguage, learningLanguage, xpGains } = await fetch(
+  `https://www.duolingo.com/2017-06-30/users/${sub}?fields=fromLanguage,learningLanguage,xpGains`,
+  {
+    headers,
+  },
+).then(response => response.json())
+
+for (let i = 0; i < process.env.LESSONS; i++) {
+  // Random Sleep
+  await new Promise(r => setTimeout(r, Math.random() * 10000))
+  
+  //Start of Script
+  const session = await fetch('https://www.duolingo.com/2017-06-30/sessions', {
+    body: JSON.stringify(SESSION_PAYLOAD),
+    headers,
+    method: 'POST',
+  }).then(response => response.json())
+
+  const response = await fetch(
+    `https://www.duolingo.com/2017-06-30/sessions/${session.id}`,
+    {
+      body: JSON.stringify({
+        ...session,
+        heartsLeft: 0,
+        startTime: (+new Date() - 60000) / 1000,
+        enableBonusPoints: false,
+        endTime: +new Date() / 1000,
+        failed: false,
+        maxInLessonStreak: 9,
+        shouldLearnThings: true,
+      }),
+      headers,
+      method: 'PUT',
+    },
+  ).then(response => response.json())
+
+  console.log({ xp: response.xpGain })
+}
